3504f8953a960ff7e8a930692f90d52c094128e9
Gabriel <gabriel_martinhoq@hotmail.com>
2025-05-06 06:43:43 -0300

    Added anotation

--- src/main/java/com/to_do_list/to_do_list/repository/UserRepository.java
+++ src/main/java/com/to_do_list/to_do_list/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.to_do_list.to_do_list.repository;
 
 import com.to_do_list.to_do_list.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
